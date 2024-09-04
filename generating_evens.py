624f71ec3239de267177cd47f0ee8bec2003bfaa
yuyu <swharriet2012@gmail.com>
2024-09-04 13:35:44 +1000

    using a loop

--- generating_evens.py
+++ generating_evens.py
@@ -9,3 +9,14 @@ print(num_list)
 def generate_evens():
     return(num_list)
 
+
+# Generating evens using a loop:
+
+# def generate_evens():
+#     result = []
+#     for x in range(1,50):
+#         if x % 2 == 0:
+#             result.append(x)
+#     return result
+
+
